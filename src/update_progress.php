3f6f6d3bc27eacdd55962cebb934b503b266ce3a
Tran Dong Duc <trandongduc92@gmail.com>
2026-04-21 19:09:08 +0700

    Update update_progress.php

--- src/update_progress.php
+++ src/update_progress.php
@@ -1,1 +1,16 @@
+<?php
+include "../config/database.php";
 
+$id = $_POST['id'];
+$progress = $_POST['progress'];
+
+$status = 'pending';
+if($progress == 100){
+    $status = 'done';
+} elseif($progress > 0){
+    $status = 'in_progress';
+}
+
+$conn->query("UPDATE tasks SET progress=$progress, status='$status' WHERE id=$id");
+
+header("Location: ../views/task_list.php");
