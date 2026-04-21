7021112efaaf91174e6d32093502548ab2f34e7a
Tran Dong Duc <trandongduc92@gmail.com>
2026-04-21 19:06:14 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,1 +1,20 @@
+<?php
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$host = 'localhost';
+$db   = 'task_manager_namlong';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+try {
+    $conn = new PDO($dsn, $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
