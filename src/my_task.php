e54dc965c070bfda0cb7b5bb0eda8851a9d93218
Tran Dong Duc <trandongduc92@gmail.com>
2026-04-30 16:39:35 +0700

    Update my_task.php

--- src/my_task.php
+++ src/my_task.php
@@ -1,1 +1,9 @@
-
+<form method="POST" action="update_task.php">
+    <input type="hidden" name="task_id" value="<?= $t['id'] ?>">
+    <select name="status">
+        <option value="pending">Chưa làm</option>
+        <option value="doing">Đang làm</option>
+        <option value="done">Hoàn thành</option>
+    </select>
+    <button>Update</button>
+</form>
